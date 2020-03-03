6b6f158827a6a49a31253fd6054c5d0e1392d373
istace.emmanuel@hotmail.com <istace.emmanuel@hotmail.com>
2020-03-03 18:31:49 +0100

    add birthday calc and fix urls

---
+++ js/age.js
@@ -0,0 +1,16 @@
+function getAge(dateString)
+{
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
+    {
+        age--;
+    }
+    return age;
+}
+
+$(document).ready(function () {
+    $(".age").text(getAge("10/07/1990"));
+});
